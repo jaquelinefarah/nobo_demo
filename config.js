beff9508487122239872ad53528bff96077897e3
Jaqueline Farah Filogonio <jackmfarah@gmail.com>
2025-10-28 03:46:32 -0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+// ============================================================
+// CONFIG FILE – Google Maps API Keys
+// ============================================================
+
+
+const CONFIG = {
+  GOOGLE_MAPS_API_KEY: "AIzaSyC0Yo6Ecj2CsZvc_BXLsHaqCtnisfYIK5Y"
+};
